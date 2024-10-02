db162a8228d03183259adaa479963d74ed2e1ba2
likevanilla <wjdgur2751@naver.com>
2024-10-02 20:15:53 +0900

    Add Game Over code

--- bomb_game/bomb_game.py
+++ bomb_game/bomb_game.py
@@ -7,11 +7,14 @@ pygame.init()  # 2. pygame 초기화
 # 3. pygame에 사용되는 전역변수 선언
 
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 size = [600, 800]
 screen = pygame.display.set_mode(size)
 
 done = False
 clock = pygame.time.Clock()
+
+game_over = False  # 게임 오버 상태를 나타내는 변수
 
 def runGame():
     bomb_image = pygame.image.load('bomb.png')
@@ -31,9 +34,10 @@ def runGame():
     person.left = size[0] // 2 - person.width // 2
     person.top = size[1] - person.height
     person_dx = 0
-    person_dy = 0
 
-    global done
+    global done, game_over
+    font = pygame.font.SysFont(None, 75)  # 게임오버 텍스트를 위한 폰트 설정
+
     while not done:
         clock.tick(30)
         screen.fill(BLACK)
@@ -42,7 +46,7 @@ def runGame():
             if event.type == pygame.QUIT:
                 done = True
                 break
-            elif event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN and not game_over:
                 if event.key == pygame.K_LEFT:
                     person_dx = -5
                 elif event.key == pygame.K_RIGHT:
@@ -53,32 +57,37 @@ def runGame():
                 elif event.key == pygame.K_RIGHT:
                     person_dx = 0
 
-        for bomb in bombs:
-            bomb['rect'].top += bomb['dy']
-            if bomb['rect'].top > size[1]:
-                bombs.remove(bomb)
-                rect = pygame.Rect(bomb_image.get_rect())
-                rect.left = random.randint(0, size[0])
-                rect.top = -100
-                dy = random.randint(3, 9)
-                bombs.append({'rect': rect, 'dy': dy})
+        if not game_over:
+            for bomb in bombs:
+                bomb['rect'].top += bomb['dy']
+                if bomb['rect'].top > size[1]:
+                    bombs.remove(bomb)
+                    rect = pygame.Rect(bomb_image.get_rect())
+                    rect.left = random.randint(0, size[0])
+                    rect.top = -100
+                    dy = random.randint(3, 9)
+                    bombs.append({'rect': rect, 'dy': dy})
 
-        person.left = person.left + person_dx
+            person.left = person.left + person_dx
 
-        if person.left < 0:
-            person.left = 0
-        elif person.left > size[0] - person.width:
-            person.left = size[0] - person.width
+            if person.left < 0:
+                person.left = 0
+            elif person.left > size[0] - person.width:
+                person.left = size[0] - person.width
 
-        screen.blit(person_image, person)
+            screen.blit(person_image, person)
 
-        for bomb in bombs:
-            if bomb['rect'].colliderect(person):
-                done = True
-            screen.blit(bomb_image, bomb['rect'])
+            for bomb in bombs:
+                if bomb['rect'].colliderect(person):
+                    game_over = True
+                screen.blit(bomb_image, bomb['rect'])
+
+        if game_over:
+            game_over_text = font.render("Game Over", True, WHITE)
+            screen.blit(game_over_text, (size[0] // 2 - game_over_text.get_width() // 2, size[1] // 2 - game_over_text.get_height() // 2))
+            pygame.display.update()
 
         pygame.display.update()
-
 
 runGame()
 pygame.quit()
