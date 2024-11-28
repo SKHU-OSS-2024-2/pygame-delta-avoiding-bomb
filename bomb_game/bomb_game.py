e5da142606009a15feb58dff7acd89c727914561
choi-day <choisschoco@gmail.com>
2024-11-28 11:00:08 +0900

    Design: 폰트 변경

--- bomb_game/bomb_game.py
+++ bomb_game/bomb_game.py
@@ -23,11 +23,24 @@ bgm_2 = 'bomb_game/sound/BGM2.wav'
 bgm_3 = 'bomb_game/sound/BGM3.wav'
 bgm_4 = 'bomb_game/sound/BGM4.wav'
 
-#font 로드
-fontObj = pygame.font.Font()
-
-#배경 이미지 로드
-background_img = pygame.image.load('bomb_game/img/background.jpg')
+#이미지 로드
+bomb_image = pygame.image.load('bomb_game/img/bomb.png').convert_alpha() # 폭탄 이미지 로드
+heart_image = pygame.image.load('bomb_game/img/heart.png').convert_alpha() # 생명 이미지 로드
+fast_image = pygame.image.load('bomb_game/img/fast.png').convert_alpha()# 번개 이미지 로드
+clock_image = pygame.image.load('bomb_game/img/Clock.png').convert_alpha()# 시계 이미지 로드
+star_image = pygame.image.load('bomb_game/img/star.png').convert_alpha()# 스타 이미지 로드
+gameover_image = pygame.image.load('bomb_game/img/gameover.png').convert_alpha()#gameover 이미지 로드
+mainlogo_image = pygame.image.load('bomb_game/img/mainlogo.png').convert_alpha()#mainlogo 이미지 로드
+background_img = pygame.image.load('bomb_game/img/background.jpg')#배경 이미지 로드
+# 특성 폭탄 이미지 로드 및 크기 조정
+slow_bomb_image = pygame.transform.scale(
+    pygame.image.load('bomb_game/img/slow_bomb.png').convert_alpha(),
+    (70, 120)  # 크기 조정
+)
+damage_bomb_image = pygame.transform.scale(
+    pygame.image.load('bomb_game/img/damage_bomb.png').convert_alpha(),
+    (70, 120)
+)
 
 # 캐릭터 애니메이션 이미지 로드
 person_idle_image = pygame.image.load('bomb_game/img/person_idle.png').convert_alpha()  # 알파 채널 활성화
@@ -40,39 +53,11 @@ person_right_images = [
     pygame.image.load('bomb_game/img/person_right2.png').convert_alpha()
 ]
 
-# 폭탄 이미지 로드
-bomb_image = pygame.image.load('bomb_game/img/bomb.png').convert_alpha()  # 알파 채널 활성화
-
-# 특성 폭탄 이미지 로드 및 크기 조정
-slow_bomb_image = pygame.transform.scale(
-    pygame.image.load('bomb_game/img/slow_bomb.png').convert_alpha(),
-    (70, 120)  # 크기 조정
-)
-
-damage_bomb_image = pygame.transform.scale(
-    pygame.image.load('bomb_game/img/damage_bomb.png').convert_alpha(),
-    (70, 120)  # 크기 조정
-)
-
-# 생명 이미지 로드
-heart_image = pygame.image.load('bomb_game/img/heart.png').convert_alpha()
-
-# 번개 이미지 로드
-fast_image = pygame.image.load('bomb_game/img/fast.png').convert_alpha()
-
-# 시계 이미지 로드
-clock_image = pygame.image.load('bomb_game/img/Clock.png').convert_alpha()
-
-# 스타 이미지 로드
-star_image = pygame.image.load('bomb_game/img/star.png').convert_alpha()
-
-#gameover 이미지 로드
-gameover_image = pygame.image.load('bomb_game/img/gameover.png').convert_alpha()
-
-#mainlogo 이미지 로드
-mainlogo_image = pygame.image.load('bomb_game/img/mainlogo.png').convert_alpha()
-
-mainlogo_image = pygame.transform.scale(mainlogo_image, (530,334))
+#폰트 설정
+gameover_font = pygame.font.Font('bomb_game/font/Pixelify_Sans/static/PixelifySans-Medium.ttf', 70)  # 게임오버 텍스트를 위한 폰트 설정
+life_font = pygame.font.Font('bomb_game/font/Pixelify_Sans/static/PixelifySans-Medium.ttf', 50)  # 목숨 표시를 위한 폰트 설정
+timer_font = pygame.font.Font('bomb_game/font/Pixelify_Sans/static/PixelifySans-Medium.ttf', 200) # 타이머에 사용할 폰트 설정
+button_font = pygame.font.Font("bomb_game/font/Pixelify_Sans/static/PixelifySans-Medium.ttf", 30) # 버튼에 사용할 폰트 설정
 
 # 마스크 생성 함수
 def create_mask(image):
@@ -98,30 +83,19 @@ fast_image = pygame.transform.scale(fast_image, (70, 91))
 clock_image = pygame.transform.scale(clock_image, (70, 70))
 slow_image = pygame.transform.scale(slow_bomb_image,(70, 90))
 damage_image = pygame.transform.scale(damage_bomb_image, (70, 90))
+mainlogo_image = pygame.transform.scale(mainlogo_image, (530,334))
 
-
+# 마스크 생성
 # 캐릭터 마스크 생성
 person_idle_mask = pygame.mask.from_surface(person_idle_image)
 person_left_masks = [pygame.mask.from_surface(img) for img in person_left_images]
 person_right_masks = [pygame.mask.from_surface(img) for img in person_right_images]
-
-# 폭탄 마스크 생성
-bomb_mask = pygame.mask.from_surface(bomb_image)
-
-# 생명 마스크 생성
-heart_mask = pygame.mask.from_surface(heart_image)
-
-#번개 마스크 생성
-fast_mask = pygame.mask.from_surface(fast_image)
-
-#시계 마스크 생성
-clock_mask = pygame.mask.from_surface(clock_image)
-
-# 스타 마스크 생성
-star_mask = pygame.mask.from_surface(star_image)
-
-#특성 폭탄 마스크 생성
-slow_mask = create_mask(slow_image)
+bomb_mask = pygame.mask.from_surface(bomb_image) # 폭탄 마스크 생성
+heart_mask = pygame.mask.from_surface(heart_image) # 생명 마스크 생성
+fast_mask = pygame.mask.from_surface(fast_image) #번개 마스크 생성
+clock_mask = pygame.mask.from_surface(clock_image) #시계 마스크 생성
+star_mask = pygame.mask.from_surface(star_image)# 스타 마스크 생성
+slow_mask = create_mask(slow_image) #특성 폭탄 마스크 생성
 damage_mask = create_mask(damage_image)
 
 # 애니메이션 관련 변수
@@ -129,9 +103,6 @@ animation_index = 0  # 현재 애니메이션 프레임 인덱스
 animation_speed = 0.2  # 애니메이션 속도 (작을수록 빠름)
 animation_timer = 0  # 프레임 전환을 위한 시간 누적
 person_image = person_idle_image  # 초기 이미지는 가만히 있는 상태
-
-# 타이머에 사용할 폰트 설정
-game_font = pygame.font.Font('bomb_game/font/Pixelify_Sans/static/PixelifySans-Medium.ttf', 200)
 
 def reset(): # 게임 상태와 관련된 변수 초기화 함수
     global done, clock, start_ticks, game_over, lives, elapsed_time
@@ -159,9 +130,8 @@ def button(msg,x,y,w,h,action=None,fcolor=WHITE): # START버튼 상세
     else:
         pygame.draw.rect(screen, BLACK, (x,y,w,h))
         fcolor=WHITE
-
-    smallTEXT = pygame.font.SysFont("bomb_game/font/Pixelify_Sans/static/PixelifySans-Medium.ttf", 30)
-    textSurf = smallTEXT.render(msg, True, fcolor)
+  
+    textSurf = button_font.render(msg, True, fcolor)
     textRect = textSurf.get_rect()
     textRect.center = ((x+(w/2)),(y+(h/2)))
     screen.blit(textSurf, textRect)
@@ -173,8 +143,6 @@ def runGame():
     global heart_spawned, last_heart_time, person_speed, fast_spawned, last_fast_time, star_spawned, last_star_time
     global clock_spawned, last_clock_time, slow_effect_active, slow_effect_end_time
     global invincible, invincible_start_time
-    invincible = False
-    invincible_start_time = 0
 
 
     try:
@@ -201,6 +169,9 @@ def runGame():
 
     slow_effect_active = False  # 폭탄 속도 감소 효과 활성화 여부
     slow_effect_end_time = 0  # 효과 종료 시간
+
+    invincible = False
+    invincible_start_time = 0
 
     heart_image = pygame.image.load('bomb_game/img/heart.png')
     heart_image = pygame.transform.scale(heart_image, (70, 70)) #생명 이미지 크기를 조절
@@ -250,9 +221,6 @@ def runGame():
     person_speed = 5 # 캐릭터 이동속도 변수
     moving = False
 
-    font = pygame.font.SysFont('https://github.com/choi-day/GDGoC_App.git', 70)  # 게임오버 텍스트를 위한 폰트 설정
-    life_font = pygame.font.SysFont('https://github.com/choi-day/GDGoC_App.git', 50)  # 목숨 표시를 위한 폰트 설정
-
     game_over_time = None #게임 오버 시간을 저장하기 위한 변수
 
     while not done:
@@ -280,7 +248,7 @@ def runGame():
         if not game_over:
             # 경과 시간 계산 및 표시
             elapsed_time = pygame.time.get_ticks() - start_ticks
-            timer = game_font.render(f"{elapsed_time // 1000} : {elapsed_time % 100:02d}", True, WHITE).convert_alpha()
+            timer = timer_font.render(f"{elapsed_time // 1000} : {elapsed_time % 100:02d}", True, WHITE).convert_alpha()
             timer.set_alpha(90) #투명도 설정 0~255
             screen.blit(timer, (size[0] // 2 - timer.get_width() // 2, size[1] // 2 - timer.get_height() // 2))
 
@@ -663,7 +631,7 @@ def runGame():
 
         # 게임 오버 시 메시지 출력
         if game_over:
-            game_over_time_text = font.render(f"Time: {game_over_time:.2f} sec", True, WHITE)
+            game_over_time_text = gameover_font.render(f"Time: {game_over_time:.2f} sec", True, WHITE)
             screen.blit(gameover_image, (size[0] // 2 - gameover_image.get_width() // 2, size[1] // 2 - gameover_image.get_height()))
             screen.blit(game_over_time_text, (size[0] // 2 - game_over_time_text.get_width() //2, size[1] // 2 + 10))
             endBtn=button("Quit", 100,525,400,100,action=True,fcolor=WHITE)
